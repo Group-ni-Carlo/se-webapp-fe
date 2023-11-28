7eaea040ecceea6a6675037d14000409b698cfd2
AJ Aparicio <anthonyjohn.aparicio-22@cpu.edu.ph>
2023-11-28 19:47:54 +0800

    build(tailwind): added colors, animation, fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,14 +7,46 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      white: '#FFFFFF',
+      orange700: '#FD9800',
+      gray100: '#D9D9D9',
+      gray300: '#BABABA',
+      gray700: '#6B6B6B',
+      blue100: '#9A99D7',
+      blue700: '#04009C',
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
-      colors: {
-        orange: '#FD9800',
+      keyframes: {
+        slideInFromLeft: {
+          '0%': {
+            transform: 'translateX(-100%)',
+            opacity: '0',
+          },
+          '100%': {
+            transform: 'translateX(0)',
+            opacity: '1',
+          },
+        },
+        slideInFromRight: {
+          '0%': {
+            transform: 'translateX(0)',
+            opacity: '0',
+          },
+          '100%': {
+            transform: 'translateX(-100%)',
+            opacity: '1',
+          },
+        },
+      },
+      animation: {
+        slideInFromLeft: 'slideInFromLeft 0.5s ease-out',
+        slideInFromRight: 'slideInFromRight 0.5s ease-out'
       },
     },
   },
