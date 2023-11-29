6e1db2b14ace012429cdfe4fb24b1343466bd36d
jedmamosto <jedmamosto@gmail.com>
2023-11-29 10:16:04 +0800

    fix: merge conflict

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,20 +7,44 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    colors: {
-      white: '#FFFFFF',
-      orange700: '#FD9800',
-      gray100: '#D9D9D9',
-      gray300: '#BABABA',
-      gray700: '#6B6B6B',
-      blue100: '#9A99D7',
-      blue700: '#04009C',
-    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        primary: {
+          100: '#FEB74D',
+          300: '#FDAD33',
+          500: '#FDA21A',
+          700: '#FD9800',
+          900: '#CA7A00',
+        },
+        secondary: {
+          100: '#9A99D7',
+          300: '#6866C3',
+          500: '#3632AF',
+          700: '#04009C',
+          900: '#03007C',
+        },
+        neutral: {
+          50: '#F5F5F5',
+          100: '#D9D9D9',
+          300: '#BABABA',
+          500: '#929292',
+          700: '#6B6B6B',
+          900: '#444444',
+        },
+        shade: {
+          dark: '#120B00',
+          light: '#FFFFFF',
+        },
+        feedback: {
+          error: '#FF9494',
+          success: '#4BB543',
+          warning: 'FF6700',
+        },
       },
       keyframes: {
         slideInFromLeft: {
@@ -49,7 +73,7 @@ const config: Config = {
         slideInFromRight: 'slideInFromRight 0.5s ease-out'
       },
     },
-  },
-  plugins: [],
+    plugins: [],
+  }
 }
 export default config
