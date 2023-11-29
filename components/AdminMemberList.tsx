4670a1b0798e6873d494496b6d1ef975b0f2a092
AJ Aparicio <anthonyjohn.aparicio-22@cpu.edu.ph>
2023-11-29 16:02:29 +0800

    chore(members): added admin management skeletons

--- components/AdminMemberList.tsx
+++ components/AdminMemberList.tsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React from 'react';
+import Icon from '@mdi/react';
+import { mdiAccount, mdiDotsHorizontal } from '@mdi/js';
+import { title, body } from './Fonts';
+
+async function getMember() {
+    const res = await fetch('/');
+}
 
 const AdminMemberList = () => {
+    const data = {
+        name: 'Anthony John Aparicio',
+        year: '2nd Year',
+        email: 'anthonyjohn.aparicio-22@cpu.edu.ph'
+    };
+    
   return (
-    <div>AdminMemberList</div>
+    <div className="flex flex-row w-full gap-4 border-y border-solid border-orange300 items-center mt-4 py-4 px-4">
+        <div className="flex">
+            <Icon className="text-gray300 border border-black rounded-3xl py-0.5" path={mdiAccount} size={1.5} />
+        </div>
+        <div className="flex flex-col w-full gap-0">
+            <span className={`${body.className} text-md`}>
+                AJ
+            </span>
+            <span className={`${body.className} text-sm text-gray300`}>
+                2nd
+            </span>
+        </div>
+        <div className="flex flex-row justify-end">
+        <Icon className="text-black" path={mdiDotsHorizontal} size={1} />
+        </div>
+    </div>
   )
 }
 
--- components/AdminMemberReq.tsx
+++ components/AdminMemberReq.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from 'react';
+import Icon from '@mdi/react';
+import { mdiAccount, mdiCheck, mdiClose } from '@mdi/js';
+import { title, body } from './Fonts';
 
 const AdminMemberReq = () => {
   return (
-    <div>AdminMemberReq</div>
+    <div className="flex flex-row w-full gap-4 border-y border-solid border-orange300 items-center mt-4 py-4 px-4">
+        <div className="flex">
+            <Icon className="text-gray300 border border-black rounded-3xl py-0.5" path={mdiAccount} size={1.5} />
+        </div>
+        <div className="flex flex-col w-full gap-0">
+            <span className={`${body.className} text-md`}>
+                Anthony John Aparicio
+            </span>
+            <span className={`${body.className} text-sm text-gray300`}>
+                anthonyjohn.aparicio-22@cpu.edu.ph
+            </span>
+        </div>
+        <div className="flex flex-row justify-end gap-4">
+            <Icon className="text-success" path={mdiCheck} size={1} />
+            <Icon className="text-error" path={mdiClose} size={1} />
+        </div>
+    </div>
   )
 }
 
