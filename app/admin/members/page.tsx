21cc4db202fe041be160345852fd4740452b7d5d
AJ Aparicio <anthonyjohn.aparicio-22@cpu.edu.ph>
2023-11-29 16:25:46 +0800

    fix(colors): adjusted classes to current palette

--- app/admin/members/page.tsx
+++ app/admin/members/page.tsx
@@ -12,13 +12,13 @@ const Members = () => {
     
   return (
     <Fragment>
-    <nav className={`flex flex-row items-center justify-center bg-white z-10 w-full border-b-2 border-gray300 border-solid ${body.className}`}>
-        <div className={`flex flex-row items-center gap-2 w-full justify-center py-2 ${activeSelection === 0 ? "bg-gradient-to-r from-orange700 via-orange500 to-orange300" : "bg-white"}`} onClick={() => setActiveSelection(0)}>
-            <Icon className="text-gray700 rounded-lg p-0.5" path={mdiAccount} size={1.05} />
+    <nav className={`flex flex-row items-center justify-center bg-shade-light z-10 w-full border-b-2 border-neutral-300 border-solid ${body.className}`}>
+        <div className={`flex flex-row items-center gap-2 w-full justify-center py-2 ${activeSelection === 0 ? "bg-gradient-to-r from-primary-700 via-primary-500 to-primary-300" : "bg-white"}`} onClick={() => setActiveSelection(0)}>
+            <Icon className="text-neutral-700 rounded-lg p-0.5" path={mdiAccount} size={1.05} />
             <h1>Members</h1>
         </div>
-        <div className={`flex flex-row items-center gap-2 w-full justify-center py-2 ${activeSelection === 1 ? "bg-gradient-to-r from-orange700 via-orange500 to-orange300" : "bg-white"}`} onClick={() => setActiveSelection(1)}>
-            <Icon className="text-gray700 rounded-lg p-0.5" path={mdiAccountCheck} size={1.05} />
+        <div className={`flex flex-row items-center gap-2 w-full justify-center py-2 ${activeSelection === 1 ? "bg-gradient-to-r from-primary-700 via-primary-500 to-primary-300" : "bg-white"}`} onClick={() => setActiveSelection(1)}>
+            <Icon className="text-neutral-700 rounded-lg p-0.5" path={mdiAccountCheck} size={1.05} />
             <h1>Requests</h1>
         </div>
     </nav>
--- components/AdminMemberReq.tsx
+++ components/AdminMemberReq.tsx
@@ -5,21 +5,21 @@ import { title, body } from './Fonts';
 
 const AdminMemberReq = () => {
   return (
-    <div className="flex flex-row w-full gap-4 border-y border-solid border-orange300 items-center mt-4 py-4 px-4">
+    <div className="flex flex-row w-full gap-4 border-y border-solid border-primary-300 items-center mt-4 py-4 px-4">
         <div className="flex">
-            <Icon className="text-gray300 border border-black rounded-3xl py-0.5" path={mdiAccount} size={1.5} />
+            <Icon className="text-neutral-300 border border-shade-dark rounded-3xl py-0.5" path={mdiAccount} size={1.5} />
         </div>
         <div className="flex flex-col w-full gap-0">
             <span className={`${body.className} text-md`}>
                 Anthony John Aparicio
             </span>
-            <span className={`${body.className} text-sm text-gray300`}>
+            <span className={`${body.className} text-sm text-neutral-300`}>
                 anthonyjohn.aparicio-22@cpu.edu.ph
             </span>
         </div>
         <div className="flex flex-row justify-end gap-4">
-            <Icon className="text-success" path={mdiCheck} size={1} />
-            <Icon className="text-error" path={mdiClose} size={1} />
+            <Icon className="text-feedback-success" path={mdiCheck} size={1} />
+            <Icon className="text-feedback-error" path={mdiClose} size={1} />
         </div>
     </div>
   )
