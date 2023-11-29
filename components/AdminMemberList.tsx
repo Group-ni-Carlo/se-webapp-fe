7436aa74abefad6b9230969d727769e72e7c70b9
AJ Aparicio <anthonyjohn.aparicio-22@cpu.edu.ph>
2023-11-29 18:00:33 +0800

    feat(admin): added member management

--- components/AdminMemberList.tsx
+++ components/AdminMemberList.tsx
@@ -1,35 +1,32 @@
+"use client"
 import React from 'react';
 import Icon from '@mdi/react';
+import { useState, useEffect } from 'react'
+import { Fragment } from 'react';
 import { mdiAccount, mdiDotsHorizontal } from '@mdi/js';
 import { title, body } from './Fonts';
 
-async function getMember() {
-    const res = await fetch('/');
-}
 
-const AdminMemberList = () => {
-    const data = {
-        name: 'Anthony John Aparicio',
-        year: '2nd Year',
-        email: 'anthonyjohn.aparicio-22@cpu.edu.ph'
-    };
+
+const AdminMemberList = (props: {name: string, year: string}) => {
+    
     
   return (
     <div className="flex flex-row w-full gap-4 border-y border-solid border-primary-300 items-center mt-4 py-4 px-4">
-        <div className="flex">
-            <Icon className="text-neutral-300 border border-shade-dark rounded-3xl py-0.5" path={mdiAccount} size={1.5} />
-        </div>
-        <div className="flex flex-col w-full gap-0">
-            <span className={`${body.className} text-md`}>
-                AJ
-            </span>
-            <span className={`${body.className} text-sm text-neutral-300`}>
-                2nd
-            </span>
-        </div>
-        <div className="flex flex-row justify-end">
-        <Icon className="text-shade-dark" path={mdiDotsHorizontal} size={1} />
-        </div>
+                <div className="flex">
+                    <Icon className="text-neutral-300 border border-shade-dark rounded-3xl py-0.5" path={mdiAccount} size={1.5} />
+                </div>
+                <div className="flex flex-col w-full gap-0">
+                    <span className={`${body.className} text-md`}>
+                        {props.name}
+                    </span>
+                    <span className={`${body.className} text-sm text-neutral-300`}>
+                        {props.year}
+                    </span>
+                </div>
+                <div className="flex flex-row justify-end">
+                    <Icon className="text-shade-dark" path={mdiDotsHorizontal} size={1} />
+                </div>
     </div>
   )
 }
--- components/AdminMemberReq.tsx
+++ components/AdminMemberReq.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import Icon from '@mdi/react';
 import { mdiAccount, mdiCheck, mdiClose } from '@mdi/js';
 import { title, body } from './Fonts';
+import { GetServerSideProps } from 'next';
 
-const AdminMemberReq = () => {
+const AdminMemberReq = (props: {name: string, email: string}) => {
   return (
     <div className="flex flex-row w-full gap-4 border-y border-solid border-primary-300 items-center mt-4 py-4 px-4">
         <div className="flex">
@@ -11,10 +12,10 @@ const AdminMemberReq = () => {
         </div>
         <div className="flex flex-col w-full gap-0">
             <span className={`${body.className} text-md`}>
-                Anthony John Aparicio
+                {props.name}
             </span>
             <span className={`${body.className} text-sm text-neutral-300`}>
-                anthonyjohn.aparicio-22@cpu.edu.ph
+                {props.email}
             </span>
         </div>
         <div className="flex flex-row justify-end gap-4">
@@ -22,7 +23,7 @@ const AdminMemberReq = () => {
             <Icon className="text-feedback-error" path={mdiClose} size={1} />
         </div>
     </div>
-  )
-}
+  ) 
+} 
 
 export default AdminMemberReq
